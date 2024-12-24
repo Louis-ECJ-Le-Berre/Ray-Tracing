e48cf7010b4603c5a5520a937f847b2ec6741ca8
Louis-ECJ-Le-Berre <louis.e.c.leberre@gmail.com>
2024-12-24 12:54:11 +0100

    Generation réussie d'une image de fond par les rayons

---
+++ main.cpp
@@ -0,0 +1,74 @@
+#include "color.h"
+#include "ray.h"
+#include "vec3.h"
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+color ray_color(const ray& r)
+{
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+}
+
+int main(int argc, char* argv[]){
+
+    std::ofstream ppm_file(argv[1]); //Créer le fichier .ppm
+    std::streambuf* original_cout_buffer = std::cout.rdbuf(ppm_file.rdbuf()); //Rediriger le std::cout vers ce fichier
+
+    //On choisit arbitrairement le ratio et la largeur
+    auto aspect_ratio = 16.0 / 9.0; 
+    int image_width = 400;
+
+    // On en déduit la hauteur
+    int image_height = int(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    // Propriétés de la caméra
+
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (double(image_width)/image_height);
+    auto camera_center = point3(0,0,0);
+
+    // Vecteurs de la caméra allant de gauche à droite et de haut en bas
+
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
+
+    // Vecteurs delta allant de pixel en pixel
+
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    // Coordonnées du pixel en haut à gauche
+
+    auto viewport_upper_left = camera_center - vec3(0,0,focal_length) -viewport_u/2 - viewport_v / 2;
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+    // Rendu
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j=0 ; j < image_height ; j++)
+    {
+        std::clog << "\rScanlines remaining : " << (image_height -j) << ' ' << std::flush;
+        for (int i = 0 ; i<image_width ; i++)
+        {
+            auto pixel_center = pixel00_loc + (i*pixel_delta_u) + (j* pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
+
+            color pixel_color = ray_color(r);
+            write_color(std::cout,pixel_color);
+        }
+
+}
+
+std::clog << "\rDone.           \n";
+std::cout.rdbuf(original_cout_buffer); //Restaurer le buffer du std::cout
+
+}
+
